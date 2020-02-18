eb633aac4f997c82a3d88bac91c5156d1c23f8b6
Pip Bbaale <namugera.philip@gmail.com>
2020-02-18 11:53:45 +0300

    welldone all colors added

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $("button#green").click(function() {
+        $("body").removeClass();
+      $("body").addClass("green-background");
+    });
+
+    $("button#yellow").click(function() {
+        $("body").removeClass();
+      $("body").addClass("yellow-background");
+    });
+
+    $("button#red").click(function() {
+        $("body").removeClass();
+      $("body").addClass("red-background");
+    });
+    $("button#blue").click(function() {
+        $("body").removeClass();
+      $("body").addClass("blue-background");
+    });
+    $("button#grey").click(function() {
+        $("body").removeClass();
+      $("body").addClass("grey-background");
+    });
+    $("button#l_blue").click(function() {
+        $("body").removeClass();
+      $("body").addClass("l_blue-background");
+    });
+    $("button#black").click(function() {
+        $("body").removeClass();
+      $("body").addClass("black-background");
+    });
+  });
